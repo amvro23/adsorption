1f0559e9a93faa551a31ee56a6183acd7f984c52
Amvro23 <91277572+amvro23@users.noreply.github.com>
2022-12-21 18:09:53 +0200

    Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+   long_description = fh.read()
+
+
+setup(
+   name='adsorption',
+   version='0.1.0.rc2',
+   description='Adsorption package.',
+   long_description=long_description,
+   author='Amvrosios Georgiadis',
+   author_email='amvro23@gmail.com',
+   packages=['adsorption'],
+   install_requires=[
+      'numpy==1.19.*',
+      'scipy>=1.7.*',
+      'pandas>=1.1.*',
+      'matplotlib',
+      ],
+)
