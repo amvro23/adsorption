a84bd7b4a04673f83f93362c0fab054345eabc0f
Amvrosios G. Georgiadis <91277572+amvro23@users.noreply.github.com>
2024-04-07 10:07:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ setup(
    author_email='amvro23@gmail.com',
    packages=['adsorption'],
    install_requires=[
-      'numpy==1.19',
-      'scipy>=1.7',
-      'pandas>=1.1',
+      'numpy==1.25.2',
+      'scipy>=1.13.0',
+      'pandas>=2.2.1',
       'matplotlib',
       ],
 )
