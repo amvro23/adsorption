4d939a2f30aacb6a1f961012be7cdd7ad98a723e
Dr. Amvrosios G. Georgiadis <91277572+amvro23@users.noreply.github.com>
2026-01-30 15:09:52 +0200

    Update __init__.py

--- adsorption/__init__.py
+++ adsorption/__init__.py
@@ -1,1 +1,28 @@
+from .models import (
+    Isotherms,
+    Kinetics,
+    ModifiedArrhenius,
+    AdsorptionDynamics,
+    AdsorptionEnthalpy,
+    IsostericHeat,
+    Adsorbent_ScaleUp,   # or AdsorbentScaleUp if you rename it
+)
 
+from .ads_data import kinetic_data, x_dyn, y_dyn, x_iheat1, y_iheat1, x_iheat2, y_iheat2
+
+__all__ = [
+    "Isotherms",
+    "Kinetics",
+    "ModifiedArrhenius",
+    "AdsorptionDynamics",
+    "AdsorptionEnthalpy",
+    "IsostericHeat",
+    "Adsorbent_ScaleUp",
+    "kinetic_data",
+    "x_dyn",
+    "y_dyn",
+    "x_iheat1",
+    "y_iheat1",
+    "x_iheat2",
+    "y_iheat2",
+]
