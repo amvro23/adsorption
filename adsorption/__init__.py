72c4734ab1404a5d990c0287fa5b98b7415d1b24
Dr. Amvrosios G. Georgiadis <91277572+amvro23@users.noreply.github.com>
2026-01-30 15:14:38 +0200

    Update __init__.py

--- adsorption/__init__.py
+++ adsorption/__init__.py
@@ -8,7 +8,7 @@ from .models import (
     Adsorbent_ScaleUp,   # or AdsorbentScaleUp if you rename it
 )
 
-from .ads_data import kinetic_data, x_dyn, y_dyn, x_iheat1, y_iheat1, x_iheat2, y_iheat2
+from .ads_data import x_kin, y_kin, x_dyn, y_dyn, x_iheat1, y_iheat1, x_iheat2, y_iheat2
 
 __all__ = [
     "Isotherms",
@@ -18,7 +18,8 @@ __all__ = [
     "AdsorptionEnthalpy",
     "IsostericHeat",
     "Adsorbent_ScaleUp",
-    "kinetic_data",
+    "x_kin",
+    "y_kin",
     "x_dyn",
     "y_dyn",
     "x_iheat1",
